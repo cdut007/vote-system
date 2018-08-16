e0bce2918fbb5bc364029ee4083ace81efc0c558
DESKTOP-R3NQDH8\dell <yongjun@ulralinked.com>
2018-08-16 11:05:39 +0800

    add info

--- app_review/src/main/java/com/itender/ms/bank/unionpay/UnionpayService.java
+++ app_review/src/main/java/com/itender/ms/bank/unionpay/UnionpayService.java
@@ -2,12 +2,14 @@ package com.itender.ms.bank.unionpay;
 
 import com.itender.ms.bank.CITIC.Ajax;
 import com.itender.ms.bank.unionpay.acp.sdk.AcpService;
+import com.itender.ms.bank.unionpay.acp.sdk.SDKConfig;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.utils.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.HashMap;
@@ -19,11 +21,17 @@ public class UnionpayService {
     private static String encoding_UTF8 = "UTF-8";
     private static final Logger logger = LoggerFactory.getLogger(UnionpayService.class);
 
+    @PostConstruct
+    public void load(){
+        SDKConfig.getConfig().loadPropertiesFromSrc();
+    }
+
+
     public Ajax queryTransationByOrderId(String orderId,Date payDate){
         Ajax ajax = new Ajax();
         Map<String, String> data = new HashMap<String, String>();
         /*** 银联全渠道系统，产品参数，除了encoding自行选择外其他不需修改 ***/
-        data.put("version", "5.0.0"); // 版本号
+        data.put("version", SDKConfig.getConfig().getVersion()); // 版本号
         data.put("encoding", encoding_UTF8); // 字符集编码
         // 可以使用UTF-8,GBK两种方式
         data.put("signMethod", "01"); // 签名方法 目前只支持01-RSA方式证书加密
@@ -32,7 +40,7 @@ public class UnionpayService {
         data.put("bizType", "000201"); // 业务类型 B2C网关支付，手机wap支付
 
         /*** 商户接入参数 ***/
-        data.put("merId", "301160173920001"); // 商户号码，请改成自己申请的商户号或者open上注册得来的777商户号测试
+        data.put("merId", SDKConfig.getConfig().getMerId()); // 商户号码，请改成自己申请的商户号或者open上注册得来的777商户号测试
         data.put("accessType", "0"); // 接入类型，商户接入固定填0，不需修改
 
         /*** 要调通交易以下字段必须修改 ***/
@@ -42,7 +50,7 @@ public class UnionpayService {
         /** 请求参数设置完毕，以下对请求参数进行签名并发送http post请求，接收同步应答报文-------------> **/
 
         Map<String, String> reqData = AcpService.sign(data, encoding_UTF8);// 报文中certId,signature的值是在signData方法中获取并自动赋值的，只要证书配置正确即可。
-        String url = "https://gateway.95516.com/gateway/api/queryTrans.do";// 交易请求url从配置文件读取对应属性文件acp_sdk.properties中的
+        String url = SDKConfig.getConfig().getSingleQueryUrl();// 交易请求url从配置文件读取对应属性文件acp_sdk.properties中的
         // acpsdk.singleQueryUrl
         // 这里调用signData之后，调用submitUrl之前不能对submitFromData中的键值对做任何修改，如果修改会导致验签不通过
         Map<String, String> rspData = AcpService.post(reqData, url, encoding_UTF8);
